d40d7ceb90b0236b3ce7a8d37898544a5c572fd4
SadingoDev <m.huch@sadingo.de>
2017-03-27 15:23:05 +0200

    Plugin Dateien

---
+++ src/Containers/ThemeContainer.php
@@ -0,0 +1,13 @@
+    <?php
+
+    namespace CeresSadingo\Containers;
+
+    use Plenty\Plugin\Templates\Twig;
+
+    class ThemeContainer
+    {
+        public function call(Twig $twig):string
+        {
+            return $twig->render('CeresSadingo::content.CeresSadingo');
+        }
+    }
