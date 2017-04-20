0a173d51bd6840cffed6ff77c91fbee091474f05
SadingoDev <m.huch@sadingo.de>
2017-04-20 12:37:54 +0200

    Startseite hinzugefügt

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -2,16 +2,26 @@
 
 namespace CeresSadingo\Providers;
 
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class CeresSadingoServiceProvider extends ServiceProvider
 {
-
 	/**
 	 * Register the service provider.
 	 */
-	public function register()
-	{
+	public function register(){
 
 	}
+
+	public function boot (Twig $twig, Dispatcher $eventDispatcher)
+  {
+    // provide template to use for homepage
+    $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
+        $container->setTemplate("CeresSadingo::Homepage.Homepage");
+        return false;
+    });
+  }
 }
