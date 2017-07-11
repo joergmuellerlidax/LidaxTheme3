1d6ee0887550a31d277d16a5107308d0d8064cdc
SadingoDev <m.huch@sadingo.de>
2017-07-11 13:24:44 +0200

    CategoryItem template

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -35,7 +35,7 @@ class CeresSadingoServiceProvider extends ServiceProvider
 		$eventDispatcher->listen('IO.init.templates', function(Partial $partial)
 		{
 			 $partial->set('footer', 'CeresSadingo::content.Footer');
-			 $partial->set('category', 'CeresSadingo::content.CategoryItem');
+			 $partial->set('CategoryItem', 'CeresSadingo::content.CategoryItem');
 		}, 0);
 		return false;
 
